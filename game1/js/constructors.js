806700bb6d1a768a1638a25597eeaa60bd7dc4c4
Gabriel <gabrieluk@cooltoad.com>
2013-02-12 13:13:52 -0800

    Create constructors.js
    some ideas...

---
+++ game1/js/constructors.js
@@ -0,0 +1,9 @@
+//ideas for how to strcture our program
+
+function Scene (zone, item, verb) {
+this.zone = zone;
+this.item = item;
+this.verb = verb;
+}
+
+var entrance = new Scene(entrance, ticket, grab);
